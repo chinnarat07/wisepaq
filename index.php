dee199a441b8b20d8c4ec0c08bb43adf7dccc5e3
KAN_J <thegoathjclan@gmail.com>
2025-02-03 10:49:06 +0700

    upd

--- index.php
+++ index.php
@@ -185,8 +185,8 @@
 
       <!-- Section Title -->
       <div class="container section-title" data-aos="fade-up">
-        <h2>Services</h2>
-        <p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
+        <h2>VIRTUALIZATION</h2>
+        <p>What We Can Do</p>
       </div><!-- End Section Title -->
 
       <div class="container" data-aos="fade-up" data-aos-delay="100">
@@ -196,10 +196,10 @@
           <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
             <div class="service-card d-flex">
               <div class="icon flex-shrink-0">
-                <i class="bi bi-activity"></i>
+                <img src="" alt="" width="50" height="50">
               </div>
               <div>
-                <h3>Nesciunt Mete</h3>
+                <h3>Sangfor HCI</h3>
                 <p>Provident nihil minus qui consequatur non omnis maiores. Eos accusantium minus dolores iure perferendis tempore et consequatur.</p>
                 <a href="service-details.html" class="read-more">Read More <i class="bi bi-arrow-right"></i></a>
               </div>
@@ -212,7 +212,7 @@
                 <i class="bi bi-diagram-3"></i>
               </div>
               <div>
-                <h3>Eosle Commodi</h3>
+                <h3>Nutanix</h3>
                 <p>Ut autem aut autem non a. Sint sint sit facilis nam iusto sint. Libero corrupti neque eum hic non ut nesciunt dolorem.</p>
                 <a href="service-details.html" class="read-more">Read More <i class="bi bi-arrow-right"></i></a>
               </div>
@@ -225,7 +225,7 @@
                 <i class="bi bi-easel"></i>
               </div>
               <div>
-                <h3>Ledo Markt</h3>
+                <h3>Microsoft</h3>
                 <p>Ut excepturi voluptatem nisi sed. Quidem fuga consequatur. Minus ea aut. Vel qui id voluptas adipisci eos earum corrupti.</p>
                 <a href="service-details.html" class="read-more">Read More <i class="bi bi-arrow-right"></i></a>
               </div>
@@ -238,7 +238,7 @@
                 <i class="bi bi-clipboard-data"></i>
               </div>
               <div>
-                <h3>Asperiores Commodit</h3>
+                <h3>VMWare</h3>
                 <p>Non et temporibus minus omnis sed dolor esse consequatur. Cupiditate sed error ea fuga sit provident adipisci neque.</p>
                 <a href="service-details.html" class="read-more">Read More <i class="bi bi-arrow-right"></i></a>
               </div>
